5f5887df914b7f9f121c6972e5460834593b33b2
MdRasel2000 <ctgmdraselislam@gmail.com>
2025-02-01 17:14:44 +0600

    js for loop

---
+++ script.js
@@ -0,0 +1,137 @@
+
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+
+
+
+
+
+        // for(i=1; 1<=6; i++){
+
+        //     console.log("hello world");
+        // }
+
+
+        // let number =[ 22, 66, 88, 88, 76, 11, 555, 33, 55, 33, 22]
+
+                 
+
+        //      for(i=0; i<=5; i++) {
+
+        //         console.log(number[i]);
+
+        //         console.log(i);
+
+        //      }    
+
+            // arry length
+
+            // let element = number.length
+
+            // for(i=0; i<element; i++){
+
+                // console.log(number[i]);
+
+
+                
+            // if(number[i]%2==0){
+
+            //     console.log("even");
+            //  }
+
+              
+            //  else{
+            //     console.log("odd");
+            //  }
+
+
+            // }
+
+
+             
+            
+// Example- pass or fail 
+let number = [66, 96, 98, 19, 30, 93, 25, 11, 9,];
+let element2 = number.length;
+
+for(i=0; i<element2; i++){
+  if(number[i]>40){
+    console.log('student is pass')
+  }
+
+  else{
+    console.log( 'student is fail')
+  }
+}
+
+//Example-Grade 
+
+let marks1 = [98, 85, 95, 85, 35,25,130.99, 68, 58, 76, 65, 222, 40, 65, 48, 60, 50 ];
+let element3 = marks1.length;
+
+for(i=0; i<element3; i++){
+  if(marks1[i]>100){
+    console.log('Invalid Number');
+  }
+
+  else if(marks1[i]>=90){
+    console.log('The grade is A+');
+  }
+
+  else if(marks1[i]>=85){
+    console.log('The grade is A');
+  }
+
+  else if(marks1[i]>=80){
+    console.log('The grade is A-');
+  }
+
+  else if(marks1[i]>=75){
+    console.log('The grade is B+');
+  }
+
+  
+  else if(marks1[i]>=70){
+    console.log('The grade is B+');
+  }
+
+  else if(marks1[i]>=65){
+    console.log('The grade is B+');
+  }
+
+  else if(marks1[i]>=60){
+    console.log('The grade is B');
+  }
+
+  else if(marks1[i]>=55){
+    console.log('The grade is B-');
+  }
+
+  else if(marks1[i]>=50){
+    console.log('The grade is C+');
+  }
+
+  else if(marks1[i]>=45){
+    console.log('The grade is C');
+  }
+
+  else if(marks1[i]>=40){
+    console.log('The grade is D');
+  }
+
+  else{
+    console.log('The grade is F')
+  }
+
+  
+
+
+}
+             
+
+
+
+
